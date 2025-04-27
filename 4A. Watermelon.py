1fd40fd13ae82abdc839ead53f38c28fe0d8ebee
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2025-04-27 18:28:09 +0300

    Add files via upload

---
+++ 4A. Watermelon.py
@@ -0,0 +1,9 @@
+# Author : Mohamed Abdelbaky 
+# Problem Link : https://codeforces.com/problemset/problem/4/A
+
+x = int(input())
+ 
+if x % 2 == 0 and x != 2:
+    print("YES")
+else:
+    print("NO")
