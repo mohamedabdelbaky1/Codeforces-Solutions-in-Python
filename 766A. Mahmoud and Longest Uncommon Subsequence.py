535b4f7d0c5f26c850ab4968a8e880c4fa2f957b
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2025-04-30 11:27:51 +0300

    Add files via upload

---
+++ 766A. Mahmoud and Longest Uncommon Subsequence.py
@@ -0,0 +1,11 @@
+# Author : Mohamed Abdelbaky 
+# Problem Link : https://codeforces.com/contest/766/problem/A
+
+x = input()
+y = input()
+
+if x==y : 
+    print(-1)
+else :
+    print(max(len(x),len(y)))
+
